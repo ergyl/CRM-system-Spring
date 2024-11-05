c642d2fd206adc42ba0e60e0574603d081028110
Erik Gylleus <144366622+ergyl@users.noreply.github.com>
2024-11-05 16:05:19 +0100

    implement getAllIncompleteActions function
    return list of actions belonging to requiredUser that are incomplete and whose "deadline" date has not passed yet

--- src/main/java/se/yrgo/services/diary/DiaryManagementServiceMockImpl.java
+++ src/main/java/se/yrgo/services/diary/DiaryManagementServiceMockImpl.java
@@ -1,27 +1,31 @@
 package se.yrgo.services.diary;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import se.yrgo.domain.Action;
 
 public class DiaryManagementServiceMockImpl implements DiaryManagementService {
-	
-	private Set<Action>allActions= new HashSet<Action>();
 
-	@Override
-	public void recordAction(Action action) {
-		// TODO Auto-generated method stub
+    private Set<Action> allActions = new HashSet<>();
 
-	}
+    @Override
+    public void recordAction(Action action) {
 
-	//Hint: 
-	//Create a list<Action>
-	//In the for each loop going through the list use this condition: "if(action.getOwningUser().equals(requiredUser) && !action.isComplete())" to add a new action to the list. 
-	public List<Action> getAllIncompleteActions(String requiredUser) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+
+    }
+
+    public List<Action> getAllIncompleteActions(String requiredUser) {
+        var actions = new ArrayList<Action>();
+        var currentDate = new GregorianCalendar();
+
+        allActions.forEach(x -> {
+            if (x.getOwningUser().equals(requiredUser)
+                    && !x.isComplete()
+                    && currentDate.before(x.getRequiredBy())) {
+                actions.add(x);
+            }
+        });
+        return actions;
+    }
 
 }
