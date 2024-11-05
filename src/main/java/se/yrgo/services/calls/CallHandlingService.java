ddbae6abaea089efedc17ea3bb13c584a68c3fbb
Erik Gylleus <144366622+ergyl@users.noreply.github.com>
2024-11-05 15:43:04 +0100

    remove comment

--- src/main/java/se/yrgo/services/calls/CallHandlingService.java
+++ src/main/java/se/yrgo/services/calls/CallHandlingService.java
@@ -7,11 +7,9 @@ import se.yrgo.domain.Call;
 import se.yrgo.services.customers.CustomerNotFoundException;
 
 public interface CallHandlingService {
-	// DO NOT IMPLEMENT THIS INTERFACE UNTIL A LATER CHAPTER!
-
-	/**
-	 * Records a call with the customer management service, and also records
-	 * any actions in the diary service
-	 */
-	public void recordCall(String customerId, Call newCall, Collection<Action> actions) throws CustomerNotFoundException;
+    /**
+     * Records a call with the customer management service, and also records
+     * any actions in the diary service
+     */
+    public void recordCall(String customerId, Call newCall, Collection<Action> actions) throws CustomerNotFoundException;
 }
