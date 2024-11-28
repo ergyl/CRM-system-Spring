090595fd0f1c6d6d3ced9dc8dda4963d63f4e996
Erik Gylleus <144366622+ergyl@users.noreply.github.com>
2024-11-28 11:53:11 +0100

    override equals and hash method in domain classes
    in order to enable testing w them

--- src/main/java/se/yrgo/domain/Call.java
+++ src/main/java/se/yrgo/domain/Call.java
@@ -1,6 +1,7 @@
 package se.yrgo.domain;
 
 import java.util.Date;
+import java.util.Objects;
 
 import javax.persistence.*;
 
@@ -9,48 +10,65 @@ import javax.persistence.*;
  * For example: Jim Called from HB at 10.30 at 15 November 2019
  */
 @Entity
-@Table(name="CUSTOMER_CALL")
+@Table(name = "CUSTOMER_CALL")
 public class Call {
 
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
 
-	@Column(name = "TIME_AND_DATE")
-	private Date timeAndDate;
+    @Column(name = "TIME_AND_DATE")
+    private Date timeAndDate;
 
-	private String notes;
+    private String notes;
 
-	public Call(String notes){
-		// this defaults to a timestamp of "now"
-		this (notes, new Date());
-	}
+    public Call(String notes) {
+        // this defaults to a timestamp of "now"
+        this(notes, new Date());
+    }
 
-	public Call(String notes, Date timestamp){
-		// this defaults to a timestamp of "now"
-		this.timeAndDate = timestamp;
-		this.notes = notes;
-	}
+    public Call(String notes, Date timestamp) {
+        // this defaults to a timestamp of "now"
+        this.timeAndDate = timestamp;
+        this.notes = notes;
+    }
 
-	public String toString()	{
-		return this.timeAndDate + " : " + this.notes;
-	}
 
-	public Date getTimeAndDate() {
-		return timeAndDate;
-	}
+    public Date getTimeAndDate() {
+        return timeAndDate;
+    }
 
-	public void setTimeAndDate(Date timeAndDate) {
-		this.timeAndDate = timeAndDate;
-	}
+    public void setTimeAndDate(Date timeAndDate) {
+        this.timeAndDate = timeAndDate;
+    }
 
-	public String getNotes() {
-		return notes;
-	}
+    public String getNotes() {
+        return notes;
+    }
 
-	public void setNotes(String notes) {
-		this.notes = notes;
-	}
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
 
-	public Call() {}
+    @Override
+    public String toString() {
+        return this.timeAndDate + " : " + this.notes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Call call = (Call) o;
+        return Objects.equals(timeAndDate, call.timeAndDate)
+                && Objects.equals(notes, call.notes);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(timeAndDate, notes);
+    }
+
+    public Call() {
+    }
 }
